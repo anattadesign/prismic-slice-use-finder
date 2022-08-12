bd53066f36e805039c53afa4dbfa2c8521a5105b
Mohammad aqib <aquibyatoo9291@gmail.com>
2022-08-12 22:42:13 +0530

    fix: build
    Signed-off-by: Mohammad aqib <aquibyatoo9291@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import App from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,5 +9,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     </ChakraProvider>
   );
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  if (appContext.ctx.res?.statusCode === 404) {
+    appContext.ctx.res.writeHead(302, { Location: "/" });
+    appContext.ctx.res.end();
+    return;
+  }
+
+  return { ...appProps };
+};
 
 export default MyApp;
