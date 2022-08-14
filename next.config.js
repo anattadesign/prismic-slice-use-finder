8a5fc7e71f5a0aace430ab31696a5b2da2c8936f
Mohammad aqib <aquibyatoo9291@gmail.com>
2022-08-14 19:16:08 +0530

    fix: code
    Signed-off-by: Mohammad aqib <aquibyatoo9291@gmail.com>

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
     REPO_NAME: "four-sigmatic",
     ACCESS_TOKEN:
       "MC5Zdllnb0JFQUFDMEFrdEVW.77-977-977-977-9fnLvv73vv70aLO-_vVBD77-9RO-_vXIr77-977-977-977-977-9fkPvv71p77-9G--_vQ9p",
-    NODE_ENV: "dev",
   },
 };
 
