b3fb2bdcc417957b1ddf1b733f75c3ae341df986
Mohammad aqib <aquibyatoo9291@gmail.com>
2022-08-16 18:08:05 +0530

    fix: Secret exposed on GitHub
    Signed-off-by: Mohammad aqib <aquibyatoo9291@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  env: {
-    REPO_NAME: "four-sigmatic",
-    ACCESS_TOKEN:
-      "MC5Zdllnb0JFQUFDMEFrdEVW.77-977-977-977-9fnLvv73vv70aLO-_vVBD77-9RO-_vXIr77-977-977-977-977-9fkPvv71p77-9G--_vQ9p",
-  },
 };
 
 module.exports = nextConfig;
